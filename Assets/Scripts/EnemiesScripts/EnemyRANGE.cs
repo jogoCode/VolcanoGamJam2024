f41f21db7bb95ba5e793c8f846b395fd20cd11d1
SpecterWolf974 <163996201+SpecterWolf974@users.noreply.github.com>
2024-11-01 23:50:09 +0400

    RANGER OP

--- Assets/Scripts/EnemiesScripts/EnemyRANGE.cs
+++ Assets/Scripts/EnemiesScripts/EnemyRANGE.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Rendering;
@@ -7,9 +8,17 @@ public class EnemyRANGE : MonoBehaviour
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _safeZone;
-    [SerializeField] private bool _inSafeZone = false;
-    
+    [SerializeField] private bool _inSafeZone;
+    [SerializeField] private GameObject _projectile;
+    [SerializeField] private GameObject _shootPoint;
+    [SerializeField] private bool _canShoot;
+    [SerializeField] private float _shootCoolDown;
 
+    private void Start()
+    {
+        _inSafeZone = false;
+        _canShoot = true;
+    }
     private void Update()
     {
         transform.LookAt(new Vector3(_player.transform.position.x,0,_player.transform.position.z));
@@ -26,12 +35,22 @@ public class EnemyRANGE : MonoBehaviour
             var direction =  _player.transform.position - gameObject.transform.position ;
             
             _agent.SetDestination(transform.position - direction);
-
-
+            StartCoroutine(Shoot());
         }
     }
     private void OnTriggerExit(Collider other)
     {
         _inSafeZone = false;
+    }
+    IEnumerator Shoot()
+    {
+        if(_canShoot == true)
+        {
+            Instantiate(_projectile,_shootPoint.transform.position,_shootPoint.transform.rotation);
+            Debug.Log(transform.rotation);
+            _canShoot = false;
+            yield return new WaitForSeconds(_shootCoolDown);
+            _canShoot = true;
+        }
     }
 }
