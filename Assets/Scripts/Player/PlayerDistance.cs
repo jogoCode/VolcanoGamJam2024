f208a5e225927131dc006c1e0c09dc7ca2f4abdb
jogoCode <163865984+jogoCode@users.noreply.github.com>
2024-11-02 10:33:15 +0400

    Distance Mode

--- Assets/Scripts/Player/PlayerDistance.cs
+++ Assets/Scripts/Player/PlayerDistance.cs
@@ -8,9 +8,13 @@ public class PlayerDistance : MonoBehaviour
 
     [SerializeField] GameObject m_bulletOrigin;
     [SerializeField] PlayerProjectile m_bullet;
+    [SerializeField] float m_shootCoolDown = 0.5f;
+    bool m_canShoot = true;
 
     public event Action OnProjectile;
 
+
+    Coroutine m_shootCoroutine;
 
     [SerializeField] PlayerController m_playerController;
    
@@ -30,7 +34,19 @@ public class PlayerDistance : MonoBehaviour
     public void CreateProjectile()
     {
         if(m_playerController.GetPlayerStateManager().GetState() == PlayerStateManager.PlayerStates.ATK) { return; }
+        if (m_playerController.GetPlayerStateManager().GetState() != PlayerStateManager.PlayerStates.DISTANCE) { return; }
+        if (!m_canShoot) { return; }
         PlayerProjectile playerProjectile = Instantiate(m_bullet,m_bulletOrigin.transform.position,m_playerController.GetModel().transform.rotation);
+        m_shootCoroutine = StartCoroutine(ShootCoolDown());
+    }
+
+    
+
+    IEnumerator ShootCoolDown()
+    {
+        m_canShoot = false;
+        yield return new WaitForSeconds(m_shootCoolDown);
+        m_canShoot = true;  
     }
     
 
