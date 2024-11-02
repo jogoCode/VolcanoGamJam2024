8344c17177fe629ace225595190fbb983f752c8c
jogoCode <163865984+jogoCode@users.noreply.github.com>
2024-11-02 11:47:32 +0400

    Projectile provoc damage

--- Assets/Scripts/HealthSysteme.cs
+++ Assets/Scripts/HealthSysteme.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 public class HealthSysteme : MonoBehaviour
 {
     [SerializeField] private int _health;
+    [SerializeField] private int _maxHealth;
     [SerializeField] private float _invincibilityFrames;
     [SerializeField] private bool _invincible;
+
+
 
     private void Start()
     {
@@ -17,6 +20,8 @@ public class HealthSysteme : MonoBehaviour
         {
             StartCoroutine(InvincibleCoolDown());
             _health -= damages;
+            _health = Mathf.Clamp(_health,0,_maxHealth);
+            CheckCanDie();
         }
     }
     IEnumerator InvincibleCoolDown()
@@ -24,5 +29,13 @@ public class HealthSysteme : MonoBehaviour
         _invincible = true ;
         yield return new WaitForSeconds(_invincibilityFrames);
         _invincible = false ;
+    }
+
+    public void CheckCanDie()
+    {
+        if(_health <= 0)
+        {
+
+        }
     }
 }
--- Assets/Scripts/Projectile/PlayerProjectile.cs
+++ Assets/Scripts/Projectile/PlayerProjectile.cs
@@ -7,7 +7,7 @@ public class PlayerProjectile : MonoBehaviour
 
     Rigidbody m_rb;
     [SerializeField] float m_projSpeed;
-
+    [SerializeField] int m_projDamage;
 
     private void Awake()
     {
@@ -26,5 +26,23 @@ public class PlayerProjectile : MonoBehaviour
         m_rb.AddForce(transform.forward*m_projSpeed,ForceMode.Impulse);
     }
 
- 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            HealthSysteme healthSysteme = other.gameObject.GetComponent<HealthSysteme>();
+
+            healthSysteme.TakeDamages(m_projDamage);
+            Oscillator oscillator = other.gameObject.GetComponent<Oscillator>();
+            if (oscillator == null) return;
+            //TODO Ajouter un KNOCK BACK
+            oscillator.StartOscillator(15);
+            m_rb.velocity = Vector3.zero;
+            m_rb.isKinematic = true;
+
+            gameObject.layer = 5;
+            transform.SetParent(other.transform);
+        }
+    }
+
 }
